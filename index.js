ed5376821ccb9fc2f76e6e2b1f2e821e7412f606
mokahoka <mayank.kathuria@1mg.com>
2019-03-22 15:23:54 +0530

    fixed multiply function not found error

--- index.js
+++ index.js
@@ -12,8 +12,13 @@ function divide(a, b) {
     return a / b;
 }
 
+function multiple(a, b){
+	return a * b;
+}
+
 module.exports = {
     add,
     subtract,
-    divide
+    divide,
+    multiple,
 };
